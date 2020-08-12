62f4132f5b9836ddd5b223893cb915518e6fba40
Gesiel Souza Oliveira <gesiel.br@gmail.com>
2020-08-12 16:47:07 -0300

    first commit

---
+++ lib/data/dunny_users.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Vanessa',
+    email: 'vanessa@alunos.com.br',
+    avatarUrl:
+        'https://scontent.fpoa8-1.fna.fbcdn.net/v/t1.0-9/73252289_2458474090873805_4609788411681701888_n.jpg?_nc_cat=111&_nc_sid=09cbfe&_nc_ohc=Ii9xnAZn3ooAX9kYPQJ&_nc_ht=scontent.fpoa8-1.fna&oh=ca02988a8a6cbd7304ac64b184983f7e&oe=5F59AD80',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Gesiel',
+    email: 'gesiel@alunos.com.br',
+    avatarUrl:
+        'https://scontent.fpoa8-2.fna.fbcdn.net/v/t1.0-9/117241695_3236124289807617_164521629722653311_o.jpg?_nc_cat=102&_nc_sid=09cbfe&_nc_ohc=GVbA9Ja-948AX87V0qH&_nc_ht=scontent.fpoa8-2.fna&oh=0fc6ffd0b4e36ad3ad92fba5a14d3e84&oe=5F583C79',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Matheus',
+    email: 'matheus@alunos.com.br',
+    avatarUrl:
+        'https://scontent.fpoa8-2.fna.fbcdn.net/v/t1.0-9/93381888_167012941434363_7181318346934059008_n.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_ohc=65J8xH8aY0UAX_Q4bKJ&_nc_ht=scontent.fpoa8-2.fna&oh=6858eb201bbafb7ec1b958620692d9f1&oe=5F59A0E8',
+  ),
+  '4': const User(
+    id: '4',
+    name: 'Guilherme',
+    email: 'guilherme@alunos.com.br',
+    avatarUrl:
+        'https://scontent.fpoa8-1.fna.fbcdn.net/v/t1.0-9/101965433_185290889594735_5669826357950248888_o.jpg?_nc_cat=105&_nc_sid=09cbfe&_nc_ohc=ra_jdw1B9IMAX-XygQA&_nc_ht=scontent.fpoa8-1.fna&oh=2dc9d2f1433d8c2d9ae48442b96596d3&oe=5F58981F',
+  )
+};
